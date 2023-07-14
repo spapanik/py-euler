b40acbaaffdbce27760ea66122246b8c564d14cc
Stephanos Kuma <stephanos@kuma.ai>
2023-07-14 16:36:17 +0100

    Change the sphinx theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,4 @@
 # noqa: INP001
-from __future__ import annotations
-
 project = "eulertools"
 project_copyright = "2022, Stephanos Kuma"
 author = "Stephanos Kuma"
@@ -9,5 +7,5 @@ extensions: list[str] = []
 templates_path = ["_templates"]
 exclude_patterns: list[str] = []
 
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
 html_static_path = ["_static"]
